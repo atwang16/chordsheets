9d45f2eb7f4dd85b877f9a1276bc306f872badb9
Austin Wang <austinw@mit.edu>
2019-04-12 02:20:25 -0400

    increased font size of slides

--- utils/headers.py
+++ utils/headers.py
@@ -9,9 +9,9 @@ Contains string versions of LaTeX file headers.
 # header for LaTeX chordsheets
 CHORDSHEET_HEADER = \
     """\\documentclass[9pt]{extarticle}
-    
+
     \\input{latex_templates/chordsheet}
-    
+
     % SET THESE FOR THE SONG
     \\newcommand{\\name}{$song} % TITLE
     \\newcommand{\\ccli}{$ccli} % CCLI
@@ -21,7 +21,7 @@ CHORDSHEET_HEADER = \
     \\newcommand{\\key}{$key} % KEY OF SONG
     \\newcommand{\\bibleverse}{$verse} % BIBLE VERSE REFERENCE
     \\newcommand{\\arranger}{$arranger} % ARRANGER
-    
+
     \\fancyhead[L]{ \\\\ \\bpm\\ bpm, \\timesignature \\\\ \\key}
     \\fancyhead[C]{{\\Large \\bf{\\name}} \\\\ \\#\\ccli \\\\ \\bibleverse}
     \\fancyhead[R]{ \\\\ \\composer \\\\ \\arranger}
@@ -29,15 +29,15 @@ CHORDSHEET_HEADER = \
 
 # header for LaTeX beamer slides
 SLIDES_HEADER = \
-    """\\documentclass[xcolor=svgnames,table,aspectratio=169]{beamer}
+    """\\documentclass[xcolor=svgnames,table,aspectratio=169,14pt]{beamer}
     \\input{latex_templates/musicslides}
-    
+
     \\newcommand{\\name}{$song}  % TITLE
     \\newcommand{\\composer}{$composer}  % COMPOSER
     \\renewcommand{\\year}{$year}  % YEAR OF PUBLISHING
     \\newcommand{\\publisher}{$publisher}  % PUBLISHER(S)
     \\newcommand{\\ccli}{$ccli}  % CCLI
-    
+
     \\renewcommand{\\cite}{\\bottomleft{``\\name'' by \\composer\\\\\\textcopyright\\year\\ \\publisher\\\\CCLI License \\#\\ccli\\\\}}
     \\renewcommand{\\header}{\\topleft{\\name}}
     """
